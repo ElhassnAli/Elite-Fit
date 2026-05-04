86fae51941e9bafd8663ec2f5e667efe07c340a0
ElhassnAli <elassn57@gmail.com>
2026-05-04 06:06:56 +0300

    #

--- src/components/weather/WeatherBanner.jsx
+++ src/components/weather/WeatherBanner.jsx
@@ -8,7 +8,7 @@ function WeatherBanner() {
   if (!weatherData.data && !isLoading) return null;
   return (
     <div
-      className={` h-60 rounded-4xl flex ${!isLoading && "md:px-10 p-5"} flex-col md:flex-row justify-between items-center md:bg-[url(./design/bg-today-large.svg)] bg-cover bg-center bg-[url(./design/bg-today-small.svg)]`}
+      className={` h-60 rounded-4xl flex ${!isLoading && "md:px-10 p-5"} flex-col md:flex-row justify-between items-center md:bg-[url(/design/bg-today-large.svg)] bg-cover bg-center bg-[url(/design/bg-today-small.svg)]`}
     >
       {isLoading ? (
         <div className="w-full h-full rounded-4xl flex justify-center flex-col gap-3 items-center bg-[#25253f]">
